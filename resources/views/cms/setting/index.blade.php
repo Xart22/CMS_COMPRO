7ecf256fbee5125b7d1e32a8ae81058d24b1c790
xart22 <fajar.rn22@gmail.com>
2021-08-18 21:55:52 +0700

    adding preview logo

--- resources/views/cms/setting/index.blade.php
+++ resources/views/cms/setting/index.blade.php
@@ -377,6 +377,40 @@
             </div>
         </div>
     </div>
+    <div class="col-md-12 col-sm-12">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2>Company Data</h2>
+                <ul class="nav navbar-right panel_toolbox">
+                    <li>
+                        <a class="collapse-link"
+                            ><i class="fa fa-chevron-up"></i
+                        ></a>
+                    </li>
+                    <li>
+                        <a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+                <table class="table">
+                    <thead>
+                        <th>Logo Small</th>
+                        <th>Logo Big</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @if(isset($data->logo_small))
+                            <td><img src="/semaju/storage/app/{{$data->logo_small}}" ></td>
+                            <td><img src="/semaju/storage/app/{{$data->logo_big}}"  ></td>
+                            @endif
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
 @section('script')
